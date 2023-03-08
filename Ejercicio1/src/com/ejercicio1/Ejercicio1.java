6a3623a539386ea0326636a6190b78eac33526c3
Edgar Chura Humora <113655611+Edgar2808-chura@users.noreply.github.com>
2023-03-07 23:56:25 -0500

    Add files via upload

---
+++ Ejercicio1/src/com/ejercicio1/Ejercicio1.java
@@ -0,0 +1,74 @@
+package com.ejercicio1;
+
+public class Ejercicio1 {
+    public static void main(String[] args){
+
+        // Tipos de Datos
+
+        // Enteros
+
+        byte num1 = 127;
+        System.out.println(num1);
+
+        short num2 = 9999;
+        System.out.println(num2);
+
+        int num3 = 999999999;
+        System.out.println(num3);
+
+        long num4 = 999999999;
+        System.out.println(num4);
+
+        // Flotantes
+
+        float num5 = 10.5f;
+        System.out.println(num5);
+
+        double num6 = 1110.5;
+        System.out.println(num6);
+
+        // Cantidad de Caracteres
+
+        char unCaracter = 'a';
+        System.out.println(unCaracter);
+
+        // Booleanos
+
+        boolean deporte = true;
+        System.out.println(deporte);
+
+        boolean videojuegos = false;
+        System.out.println(videojuegos);
+
+        // Strings
+
+        String nombre = "Edgar";
+        System.out.println(nombre);
+
+        String apellido = "Chura";
+        System.out.println(apellido);
+
+        String nombreCompleto = nombre + " " + apellido;
+        System.out.println(nombreCompleto);
+
+        String telefono = null;
+        System.out.println(telefono);
+
+        // Envoltorio
+
+        Integer num10 = null;
+        System.out.println(num10);
+
+        Integer num11 = 10;
+        System.out.println(num11);
+
+        Long num12 = null;
+        System.out.println(num12);
+
+        Long num13 = 10L;
+        System.out.println(num13);
+
+        System.out.println("Fin del proceso");
+    }
+
+}
